a19cddad73bbf703eb5ce8bb63d4b6b937ac1c85
Dominus8 <ikari162@gmail.com>
2022-12-20 20:25:11 +0300

    + bootstrap

--- index.php
+++ index.php
@@ -5,16 +5,26 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     <title>E-note</title>
 </head>
 
 <body>
-    <h1>E-note.group</h1>
-    <h2>
-        It для людей.
-    </h2>
-    <p>Наличие товаров можете уточнить по телефону +74993502608 или написав на почту info@enote-shop.ru</p>
+    <div class="row">
+        <div class="col-md-12">
+            <h1>E-note.group</h1>
+            <h2>
+                It для людей.
+            </h2>
+            <p>Наличие товаров можете уточнить по телефону +74993502608 <br> Или написав на почту info@enote-shop.ru</p>
+        </div>
+    </div>
 
+
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 </body>
 
 </html>
