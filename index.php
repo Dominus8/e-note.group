5ab4024206e73601ab6c9de04f4df49f57efc747
Dominus8 <ikari162@mail.ru>
2022-12-20 15:00:08 +0300

    1-st commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E-note</title>
+</head>
+
+<body>
+    <h1>E-note.group</h1>
+    <h2>
+        It для людей.
+    </h2>
+    <p>Наличие товаров можете уточнить по телефону +74993502608 или написав на почту info@enote-shop.ru</p>
+
+</body>
+
+</html>
